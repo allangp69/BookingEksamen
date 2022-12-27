f70423238065fa3bb47e474d249c18d62cc45554
Allan Pedersen <ap@airsupport.dk>
2022-12-27 14:27:08 +0100

    Moved BookingEksamenWebUI project to the correct folder: BookingEksamenWebUI

---
+++ BookingEksamenWebUI/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using BookingEksamenWebUI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingEksamen.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Comment> Comments { get; set; }
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<IdentityRole>().HasData(new List<IdentityRole>
+        //     {
+        //         new IdentityRole {
+        //             Id = Guid.NewGuid().ToString(),
+        //             Name = "Admin", 
+        //             NormalizedName = "ADMIN"
+        //         },
+        //         new IdentityRole {
+        //             Id = Guid.NewGuid().ToString(),
+        //             Name = "Booker", 
+        //             NormalizedName = "BOOKER"
+        //         },
+        //         new IdentityRole {
+        //             Id = Guid.NewGuid().ToString(),
+        //             Name = "Artist", 
+        //             NormalizedName = "ARTIST"
+        //         }
+        //     });
+        // }
+    }
+}
