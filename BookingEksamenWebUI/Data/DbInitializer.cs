be52da17bdc99b2ef5aaa062ae7b701a2b00435b
Allan Pedersen <ap@airsupport.dk>
2022-12-28 09:33:55 +0100

    Adding roles to the database in the DbInitializer.cs

--- BookingEksamenWebUI/Data/DbInitializer.cs
+++ BookingEksamenWebUI/Data/DbInitializer.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace BookingEksamen.Data;
 
 public static class DbInitializer
 {
-    public static void Initialize(ApplicationDbContext context)
+    public static void Initialize(ApplicationDbContext context, RoleManager<IdentityRole> roleManager)
     {
         context.Database.EnsureCreated();
+        Seed(roleManager);
+    }
+
+    private static void Seed(RoleManager<IdentityRole> roleManager)
+    {
+        roleManager.CreateAsync(new IdentityRole("Admin"));
+        roleManager.CreateAsync(new IdentityRole("Booker"));
+        roleManager.CreateAsync(new IdentityRole("Artist"));
     }
 }
