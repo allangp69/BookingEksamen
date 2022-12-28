a68d5e40f69080de6e64b1fd470c374b944a467c
Allan Pedersen <ap@airsupport.dk>
2022-12-28 09:36:34 +0100

    Registering the CommentRepository in dependency injection at startup

--- BookingEksamenDataManager/Program.cs
+++ BookingEksamenDataManager/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddTransient<IArtists, ArtistRepository>();
 builder.Services.AddTransient<IBookers, BookerRepository>();
+builder.Services.AddTransient<IComments, CommentRepository>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
