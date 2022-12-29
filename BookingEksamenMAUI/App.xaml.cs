02bcdd7c23cab4872e7b22fdcc2708125955de6c
Allan Pedersen <ap@airsupport.dk>
2022-12-29 08:05:44 +0100

    Added MAUI project

---
+++ BookingEksamenMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingEksamenMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
