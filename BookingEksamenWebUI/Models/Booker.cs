91ba6b607a7376670379393f50d6723627be2aa6
Allan Pedersen <ap@airsupport.dk>
2022-12-30 12:08:18 +0100

    Added Email and list of artists (dummies as default) to Booker class

--- BookingEksamenWebUI/Models/Booker.cs
+++ BookingEksamenWebUI/Models/Booker.cs
@@ -2,9 +2,19 @@
 
 public class Booker
 {
+    public Booker()
+    {
+        Artists = new List<Artist>
+        {
+            new Artist{Name="Artist1"},
+            new Artist{Name="Artist2"}
+        };
+    }
     public int BookerID { get; set; }
     public string? Name { get; set; }
     public string? LoginID { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime ModifiedDate { get; set; }
+    public string? Email { get; set; }
+    public IEnumerable<Artist> Artists { get; set; }
 }
